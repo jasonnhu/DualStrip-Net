23dfd1b61d5ee25f27af0ec165e92beaefe7b083
Jt <jthu2019@gmail.com>
2025-11-16 23:44:44 +0800

    Add files via upload

---
+++ train.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+now=$(date +"%Y%m%d_%H%M%S")
+# modify these augments if you want to try other datasets, splits or methods
+# dataset: ['levir', 'whu']
+# method: ['unimatch', 'supervised']
+# exp: just for specifying the 'save_path'
+# split: ['5%', '10%', '20%', '40%']
+dataset='mass'
+method='dualstrip'
+exp='dualstrip-net' #deeplabv3plus_r50
+split='20%'
+
+config=configs/${dataset}.yaml
+labeled_id_path=splits/$dataset/$split/labeled.txt
+unlabeled_id_path=splits/$dataset/$split/unlabeled.txt
+save_path=exp/$dataset/$method/$exp/$split
+
+mkdir -p $save_path
+
+python $method.py \
+    --config=$config --labeled-id-path $labeled_id_path --unlabeled-id-path $unlabeled_id_path \
+    --save-path $save_path
